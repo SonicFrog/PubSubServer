80c22980a8b55133cca74cf3eb8bd1cf5eeae087
tOverney <tristan.overney@epfl.ch>
2014-03-30 14:55:20 +0200

    Acceptor modifié pour respecté la section "Etape 4".

--- src/concurrence/TCPAcceptor.java
+++ src/concurrence/TCPAcceptor.java
@@ -1,11 +1,11 @@
 package concurrence;
 
 import java.io.IOException;
-
 import java.net.ServerSocket;
 import java.net.Socket;
-
 import java.util.ArrayList;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TCPAcceptor implements Runnable {
 
@@ -13,15 +13,21 @@ public class TCPAcceptor implements Runnable {
 	
 	public static final int PORT = 7676;
 	
+	private static final int MAX_NB_THREAD = 12;
+	
 	private ServerSocket socket;
+	
+	private ExecutorService tPool;
 	
 	private MessageBuffer buffer = new MessageBuffer();
 	
-	private ArrayList<TCPReader> clients = new ArrayList<>();
+	// private ArrayList<TCPReader> clients = new ArrayList<>();
+	// TODO sauf erreur pas besoin de garder une listes des clients dans le "welcoming thread"
 	
 	private TCPAcceptor() {
 		try {
 			socket = new ServerSocket(PORT);
+			tPool = Executors.newFixedThreadPool(MAX_NB_THREAD);
 		} catch(IOException e) {
 			System.err.println("Unable to open socket on port " + PORT + ": " + e.getMessage());
 			System.exit(-1);
@@ -40,17 +46,16 @@ public class TCPAcceptor implements Runnable {
 	 */
 	public void run() {
 		Socket client;
-		TCPReader chandler;
-		Thread tclient;
-		int cnum = 0;
+		// TCPReader chandler;   c.f l.15
+		//Thread tclient;
+		int cid = 0;
 		
 		try {
 			while(true) {
 				client = socket.accept();
-				chandler = new TCPReader(cnum++, client, buffer);
-				clients.add(chandler);
-				tclient = new Thread(chandler);
-				tclient.start();
+				// chandler = new TCPReader(cid++, client, buffer);
+				// clients.add(chandler); c.f. l.15
+				tPool.execute(new TCPReader(cid++, client, buffer));
 			}			
 		} catch(IOException e) {
 			System.err.println("Error while client connecting: " +  e.getLocalizedMessage());
