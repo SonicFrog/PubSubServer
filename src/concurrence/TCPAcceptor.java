c5706dc88660b7ded380e23d98a833100a61bce2
Ogier Bouvier <ogier.bouvier@epfl.ch>
2014-04-02 18:29:21 +0200

    Changed the message handling from one MessageBroker to multiple CommandHandlers

--- src/concurrence/TCPAcceptor.java
+++ src/concurrence/TCPAcceptor.java
@@ -12,20 +12,30 @@ public class TCPAcceptor implements Runnable {
 	
 	public static final int PORT = 7676;
 	
-	private static final int MAX_NB_THREAD = 12;
+	public static final int MAX_NB_THREAD = 12;
 	
 	private ServerSocket socket;
 	
 	private ExecutorService tPool;
 	
+	private Thread[] handlers = new Thread[MAX_NB_THREAD];
+	
 	private MessageBuffer buffer = new MessageBuffer();
 	
 	private TCPAcceptor() {
 		try {
+			System.err.println(getClass().getName()  + ": Binding socket on localhost:" + PORT + "...");
 			socket = new ServerSocket(PORT);
+			
+			
+			System.err.println(getClass().getName() +": Instantiating command handlers...");
+			for(int i = 0 ; i < handlers.length ; i++) {
+				handlers[i] = new Thread(new CommandHandler(buffer));
+				handlers[i].start();
+			}
 			tPool = Executors.newFixedThreadPool(MAX_NB_THREAD);
 		} catch(IOException e) {
-			System.err.println("Unable to open socket on port " + PORT + ": " + e.getMessage());
+			System.err.println(getClass().getName() + ": Unable to open socket on port " + PORT + ": " + e.getMessage());
 			System.exit(-1);
 		}
 	}
@@ -47,10 +57,11 @@ public class TCPAcceptor implements Runnable {
 		try {
 			while(true) {
 				client = socket.accept();
+				System.err.println(getClass().getName() + ": New client connected " + cid);
 				tPool.execute(new TCPReader(cid++, client, buffer));
 			}			
 		} catch(IOException e) {
-			System.err.println("Error while client connecting: " +  e.getLocalizedMessage());
+			System.err.println(getClass().getName() + ": Error while client connecting: " +  e.getLocalizedMessage());
 		}
 	}
 }
