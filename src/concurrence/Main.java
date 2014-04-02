8760897c9a01d91010cc5a2c809a058ae22523c8
Ogier Bouvier <ogier.bouvier@epfl.ch>
2014-04-02 18:23:00 +0200

    Added code to start the server from the main method

--- src/concurrence/Main.java
+++ src/concurrence/Main.java
@@ -6,6 +6,13 @@ package concurrence;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		TCPAcceptor server = TCPAcceptor.getServer();
+		Thread main = new Thread(server);
+		main.start();
+		try {
+			main.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
